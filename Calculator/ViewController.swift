c6c299ddaf09819931b3fea61e1c2ff02500e13a
Ulrich Schwarz <ulischwarz2000@arcor.de>
2016-04-23 18:06:38 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ulrich Schwarz on 23.04.16.
+//  Copyright © 2016 Ulrich Schwarz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
